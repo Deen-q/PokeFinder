b738e0902dde5f5a07dbdc1a90d95124a937d40d
Deen-q <124711768+Deen-q@users.noreply.github.com>
2023-07-15 17:56:36 +0100

    Huge progress. A name appear for each card

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -1,13 +1,9 @@
-import React from 'react'
+import React from 'react';
 
-export default function Card({pokemon}) {
-  // console.log(pokemon)
+export default function Card({ pokemon }) {
   return (
     <div className='Card_js'>
-        <h2>1</h2>
-        {/* <img src={"./images/ShrekRedEyes.jpg"} alt=""/> */}
-        <h2 key={pokemon}>{pokemon}</h2> {/*Not pokemon.name because pokemon is an array*/}
-        
+      <h2>{pokemon}</h2>
     </div>
-  )
+  );
 }
--- src/Components/Main.js
+++ src/Components/Main.js
@@ -4,72 +4,39 @@ import PokeInfo from './PokeInfo';
 import axios from 'axios';
 
 export default function Main() {
-  
   const [pokemon, setPokemon] = useState([]);
   const [currentPageUrl, setCurrentPageUrl] = useState("https://pokeapi.co/api/v2/pokemon/");
-  const [nextPageUrl, setNextPageUrl] = useState()
-  const [prevPageUrl, setPrevPageUrl] = useState()
-  const [loading, setLoading] = useState(true); //loading message
+  const [nextPageUrl, setNextPageUrl] = useState();
+  const [prevPageUrl, setPrevPageUrl] = useState();
+  const [loading, setLoading] = useState(true);
 
-  
-  
-  useEffect(() =>{
-    setLoading(true)
+  useEffect(() => {
+    setLoading(true);
     axios.get(currentPageUrl).then(res => {
-      setLoading(false) // set to true to test it OR comment out
-      setNextPageUrl(res.data.next)
-      setPrevPageUrl(res.data.previous)
-      setPokemon(res.data.results.map(p => p.name)) //res.data, where data is the Object
-    })
-  }, [currentPageUrl]) // IF url changes, rerun the code inside useEffect --> re-get the pokemon on every page
+      setLoading(false);
+      setNextPageUrl(res.data.next);
+      setPrevPageUrl(res.data.previous);
+      const pokemonNames = res.data.results.map(p => p.name);
+      setPokemon(pokemonNames);
+    });
+  }, [currentPageUrl]);
 
-  if (loading) return "Loading..."
-
+  if (loading) return "Loading...";
 
   return (
     <div className="Main_js_container">
       <div className="left-content">
-        <Card pokemon={pokemon}/>
-
+        {pokemon.map((p, index) => (
+          <Card key={index} pokemon={p} />
+        ))}
         <div className="btn-div">
           <button>Previous</button>
           <button>Next</button>
         </div>
-
       </div>
-
       <div className="right-content">
         <PokeInfo />
-
       </div>
     </div>
-  )
+  );
 }
-
-////// OLD
-// const getPokemon = async(res) => {
-  //   res.map(async(item) => {
-  //     // console.log(item.url)
-  //     const result =  await axios.get(item.url)
-  //     console.log(result.data);
-  //     setPokeData(state=>{
-  //       state=[...state, result.data]
-  //       return state
-  //     })
-  //   })
-  // }
-
-  // useEffect(() => {
-  //   pokemonFunction();
-
-  // }, [url]) // research this part a bit more
-  // //if [] empty = only render once page loads
-  // //otherwise: when url UPDATES --> this useEffect runs = RENDER AGAIN
-
-  // const pokemonFunction = async() => {
-    //   setLoading(true);
-    //   const res = await axios.get(url);
-    //   // console.log(res.data.results)
-    //   getPokemon(res.data.results)
-    //   setLoading(false);
-    // }
