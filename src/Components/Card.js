ac543be6de04eb98c815790605f3a39739b46a79
Deen-q <124711768+Deen-q@users.noreply.github.com>
2023-07-15 19:50:06 +0100

    Added comments and updated useEffect fetch request to the most common v of JS

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -3,6 +3,7 @@ import React from 'react';
 export default function Card({ pokemon }) {
   return (
     <div className='Card_js'>
+      {/* <img src={pokemon.sprites.front_default} alt="pokemon"></img> */}
       <h2>{pokemon}</h2>
     </div>
   );
