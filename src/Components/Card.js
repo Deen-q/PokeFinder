7c54dca9075f9c558fd019b2e9f53d53fdfe1595
Deen-q <124711768+Deen-q@users.noreply.github.com>
2023-07-17 17:24:21 +0100

    Added button functionality and prevent previous button from appearing when there is no previous page available i.e., on page 1. Tidied up the comments and added more where necessary.

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default function Card({ pokemon }) {
   return (
     <div className="Card_js">
-      <h2>{pokemon.name}</h2>
+      <h2>{pokemon.name}</h2> {/*before pokemon.name didnt work because then, pokemon = array, I think*/}
       <img src={pokemon.sprites.front_default} alt="pokemon" />
       {/* Display other desired information from pokemon */}
     </div>
--- src/Components/Main.js
+++ src/Components/Main.js
@@ -6,30 +6,50 @@ import axios from 'axios';
 export default function Main() {
   const [pokemon, setPokemon] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [currentUrl, setCurrentUrl] = useState("https://pokeapi.co/api/v2/pokemon/");
+  const [nextPageState, setNextPageState] = useState();
+  const [prevPageState, setPrevPageState] = useState();
+
 
   useEffect(() => {
+    let cancel // undefined, handled using the CancelToken
     const fetchData = async () => {
-      try {
+      try { // needed for error handling, below
         setLoading(true);
-        const response = await axios.get("https://pokeapi.co/api/v2/pokemon/");
-        // console.log(response)
+
+        // response -> overall data from initial API call
+        const response = await axios.get(currentUrl, {
+          cancelToken: new axios.CancelToken(c => cancel = c)
+        });
+       
         const results = response.data.results;
-        console.log(response.data);
-        const pokemonData = await Promise.all(results.map(async (p) => { //map iterates over each URL + creates new array with corresponding promises
-          // 'async(p)=>' p for parameter, p = each element URL in 'results' array
-          // everything after the '{' for (p) =>, is just code to be executed for every iteration of map method
+        const nextPage = response.data.next
+        const prevPage = response.data.previous
+
+    // 'async(p)=>' p for parameter, p = each element URL in 'results' array. Everything after => is executed for every .map iteration
+        const entirePokemonArrayData = await Promise.all(results.map(async (p) => { 
+          //map iterates over each URL + creates new array with corresponding promises
+          
+          // pokemonRes = response data for INDIVIDUAL Pokemon API call
           const pokemonRes = await axios.get(p.url); //p.url selects the url, instead of name (the only 2 available, 'url' or 'name')
+
           return pokemonRes.data;
         }));
+
         setLoading(false);
-        setPokemon(pokemonData);
+        setPokemon(entirePokemonArrayData); // !!! useState var 'pokemon' is now = to pokemonData
+        setPrevPageState(prevPage)
+        setNextPageState(nextPage)
+        
       } catch (error) {
-        // console.error(error);
+        console.error(error, "Uh-oh, useEffect Error");
+        // Everything written in this error block is what code will be executed in the event there is an error.Good backup solution.
       }
     };
 
     fetchData();
-  }, []);
+    return () => cancel() //prevents old data from loading on top of newer data
+  }, [currentUrl]); // end of useEffect
 
   if (loading) return "Loading...";
 
@@ -38,11 +58,24 @@ export default function Main() {
       <div className="left-content">
         {pokemon.map((p, index) => (
           <Card key={index} pokemon={p} />
-        ))}
+        ))} {/*.map creates a card per pokemon in the array
+        ->for each element, in pokemon array --> execute the arrow function --> returns Card component with pokemon PROP
+        ---> pokemon prop/pokemon data, symbolised as p for every iteration
+        ----> p is a VARIABLE that holds value of the current pokemon data being processed by each iteration of .map function
+        -> each card receives a different pokemon object as the pokemon prop*/}
+        
         <div className="btn-div">
-          <button>Previous</button>
-          <button>Next</button>
+          {prevPageState && ( // prevPageState has a truthy value only when previous page URL is available. It does not on page 1. 
+            <button onClick={()=> { setCurrentUrl(prevPageState)
+          }} >Previous
+          </button> )}
+
+          <button
+          onClick={()=> {
+            setCurrentUrl(nextPageState)
+          }} >Next</button>
         </div>
+        
       </div>
       <div className="right-content">
         <PokeInfo />
