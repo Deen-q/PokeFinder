fda6eee34f89a41b722de0d6b6f67633e839f010
Deen-q <124711768+Deen-q@users.noreply.github.com>
2023-07-22 18:07:07 +0100

    And that's 'technically' the project complete! By this I mean the basic app fulfils it's function. But there's still a lot more I want to build in. Did not expect this to be as fun as I thought it would!

--- src/Components/PokemonInfo.js
+++ src/Components/PokemonInfo.js
@@ -1,17 +1,29 @@
 import React from 'react'
 
 export default function PokemonInfo({ selectedPokemon }) {
-  if (!selectedPokemon) {
-    return <div>No Pokemon selected</div>;
-  }
+  if (!selectedPokemon) { // if falsy
+    return <div><h2>No Pokemon selected</h2></div>;
+  } //reminder: code not executed if this return is fulfilled!
+  //Thus, bypasses the issue when selectedPokemon = null
 
   const capitalisedName = selectedPokemon.name.charAt(0).toUpperCase() + selectedPokemon.name.slice(1) // This had to be BELOW the conditional check...
 
+
   return (
     <div>
-      <h1>Selected Pokemon Info</h1>
+      <h2>Selected Pokemon Info</h2>
       <p>{capitalisedName}</p>
-      {/* Add other information as needed */}
+      <img 
+      className="OfficialArtwork"
+      src={selectedPokemon.sprites.other['official-artwork'].front_default} 
+      alt={`Official Artwork for ${selectedPokemon}`}
+      />
+      <h2>HP:{selectedPokemon.stats[0].base_stat}</h2>
+      <h2>Attack:{selectedPokemon.stats[1].base_stat}</h2>
+      <h2>Defense:{selectedPokemon.stats[2].base_stat}</h2>
+      <h2>Sp. Attack:{selectedPokemon.stats[3].base_stat}</h2>
+      <h2>Sp. Defense:{selectedPokemon.stats[4].base_stat}</h2>
+      <h2>Speed:{selectedPokemon.stats[5].base_stat}</h2>
       </div>
       );
     }
