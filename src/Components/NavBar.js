d293f4356b8424a9b2a86b0871799f01ca98e3b0
Deen-q <124711768+Deen-q@users.noreply.github.com>
2023-11-10 17:13:54 +0000

    Added GitHub icon

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from "react";
+import GitHubLogo from "../images/GitHubLogo.png";
 
 export default function NavBar() {
   return (
-    <div className='NavBar'>
+    <>
+      <div className="NavBar">
         <h2>PokéFinder</h2>
-    </div>
-  )
+        <a
+          href="https://github.com/Deen-q/PokeFinder"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img src={GitHubLogo} alt="GitHubIcon" />
+        </a>
+      </div>
+    </>
+  );
 }
